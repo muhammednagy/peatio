0545b5c911b3a853c96ec07805c935761c224cf5
Dicky Trifadillah <dicky.trifadillah@doterb.com>
2017-12-21 17:18:24 +0700

    webhook update

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,15 +3,6 @@ class WebhooksController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 	def tx
 		if params[:type] == "transaction" && params[:hash].present?
-      transactions = exec "bitcoin-cli -rpcport=19332 listtransactions"
-      puts transactions
-      # txid = accounts.map{ |p| p[:txid] if p[:txid] == params[:hash] }.compact.join
-      current_transaction = transactions.select{ |tr| tr[:txid] == params[:hash] }[0]
-      puts current_transaction
-      account = PaymentAddress.find_by_address(current_transaction[:address]).account
-      current_balance = account.balance
-      account.update(balance: current_balance + current_transaction[:amount])
-      puts current_balance
 			AMQPQueue.enqueue(:deposit_coin, txid: params[:hash], channel_key: "satoshi")
 			render :json => { :status => "queued" }
 		end
