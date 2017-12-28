8718dfceef40fee896a01884c258909b55b5ca82
Dicky Trifadillah <dicky.trifadillah@doterb.com>
2017-12-28 15:44:06 +0700

    update environment lograge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Peatio::Application.configure do
   config.lograge.enabled = false
-  config.lograge.formatter = ->(data) { LogWebsite.create(data) } # data is a ruby hash
 
   # Settings specified here will take precedence over those in config/application.rb
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Peatio::Application.configure do
-  config.lograge.enabled = false
+  config.lograge.enabled = true
+  config.lograge.formatter = ->(data) { LogWebsite.create(data) } # data is a ruby hash
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests.
